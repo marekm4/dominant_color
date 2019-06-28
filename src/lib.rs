3b32821474c1983ed9d3304c491d93a74d5981dc
Marek Michalik <marekmichalik43@gmail.com>
2019-06-28 22:11:27 +0200

    Test single pixel image

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,24 @@
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct Color {
-    r: u8,
-    g: u8,
-    b: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
 }
 
 pub fn get_colors(pixels: &[u8]) -> Vec<Color> {
-    let colors: Vec<Color> = Vec::new();
+    get_colors_with_config(&pixels, 224.0, 0.01)
+}
+
+pub fn get_colors_with_config(pixels: &[u8], down_size_to: f64, small_bucket: f64) -> Vec<Color> {
+    let mut colors: Vec<Color> = Vec::new();
+
+    if pixels.len() >= 3 {
+        colors.push(Color {
+            r: pixels[0],
+            g: pixels[1],
+            b: pixels[2],
+        });
+    }
+
     colors
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,24 @@
 extern crate dominant_color;
 
+use dominant_color::Color;
+
 #[test]
 fn empty_image() {
     let pixels: [u8; 0] = [];
     let colors = dominant_color::get_colors(&pixels);
     assert_eq!(colors.len(), 0);
+}
+
+#[test]
+fn single_pixel() {
+    let pixels: [u8; 3] = [135, 202, 82];
+    let colors = dominant_color::get_colors(&pixels);
+    assert_eq!(
+        colors[0],
+        dominant_color::Color {
+            r: 135,
+            g: 202,
+            b: 82
+        }
+    );
 }

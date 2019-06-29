dc6231bb76644e23dad7c8441b2144616b91f0cb
Marek Michalik <marekmichalik43@gmail.com>
2019-06-29 10:55:07 +0200

    Implement algorithm for RBG palette

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,75 @@ pub struct Color {
     pub b: u8,
 }
 
+#[derive(Copy, Clone, Debug)]
+struct Bucket {
+    r: f64,
+    g: f64,
+    b: f64,
+    count: f64,
+}
+
 pub fn get_colors(pixels: &[u8]) -> Vec<Color> {
-    get_colors_with_config(&pixels, 224.0, 0.01)
+    get_colors_with_config(&pixels, 224.0 * 224.0, 0.01)
 }
 
 pub fn get_colors_with_config(pixels: &[u8], down_size_to: f64, small_bucket: f64) -> Vec<Color> {
-    let mut colors: Vec<Color> = Vec::new();
+    // calculate down size step
+    let pixel_count = pixels.len() / 3;
+    let step = (pixel_count / down_size_to.round() as usize).max(1);
 
-    if pixels.len() >= 3 {
-        colors.push(Color {
-            r: pixels[0],
-            g: pixels[1],
-            b: pixels[2],
-        });
+    // load pixels to buckets
+    let mut buckets = [[[Bucket {
+        r: 0.0,
+        g: 0.0,
+        b: 0.0,
+        count: 0.0,
+    }; 2]; 2]; 2];
+    for i in (0..pixels.len()).step_by(3 * step) {
+        let r = pixels[i];
+        let g = pixels[i + 1];
+        let b = pixels[i + 2];
+        let i = (r >> 7) as usize;
+        let j = (g >> 7) as usize;
+        let k = (b >> 7) as usize;
+        buckets[i][j][k].r += r as f64;
+        buckets[i][j][k].g += g as f64;
+        buckets[i][j][k].b += b as f64;
+        buckets[i][j][k].count += 1.0;
+    }
+
+    // calculate buckets averages
+    let mut buckets_averages: Vec<Bucket> = Vec::new();
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                let current_bucket = &buckets[i][j][k];
+                if current_bucket.count > 0.0 {
+                    buckets_averages.push(Bucket {
+                        r: current_bucket.r / current_bucket.count,
+                        g: current_bucket.g / current_bucket.count,
+                        b: current_bucket.b / current_bucket.count,
+                        count: current_bucket.count,
+                    })
+                }
+            }
+        }
+    }
+
+    // sort buckets averages
+    buckets_averages.sort_by(|a, b| b.count.partial_cmp(&a.count).unwrap());
+
+    // convert buckets to colors, ignore small buckets
+    let mut colors: Vec<Color> = Vec::new();
+    let sampled_pixel_count = pixel_count / step;
+    for ba in &buckets_averages {
+        if ba.count / sampled_pixel_count as f64 > small_bucket {
+            colors.push(Color {
+                r: ba.r.round() as u8,
+                g: ba.g.round() as u8,
+                b: ba.b.round() as u8,
+            });
+        }
     }
 
     colors
--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,7 @@
 extern crate dominant_color;
+extern crate image;
 
-use dominant_color::Color;
+use std::path;
 
 #[test]
 fn empty_image() {
@@ -19,6 +20,84 @@ fn single_pixel() {
             r: 135,
             g: 202,
             b: 82
+        }
+    );
+}
+
+#[test]
+fn one_color() {
+    let pixels: [u8; 6] = [135, 202, 82, 135, 202, 82];
+    let colors = dominant_color::get_colors(&pixels);
+    assert_eq!(
+        colors[0],
+        dominant_color::Color {
+            r: 135,
+            g: 202,
+            b: 82
+        }
+    );
+}
+
+#[test]
+fn two_colors() {
+    let pixels: [u8; 9] = [255, 0, 0, 0, 255, 0, 255, 0, 0];
+    let colors = dominant_color::get_colors(&pixels);
+    assert_eq!(colors[0], dominant_color::Color { r: 255, g: 0, b: 0 });
+    assert_eq!(colors[1], dominant_color::Color { r: 0, g: 255, b: 0 });
+}
+
+#[test]
+fn mixed_colors() {
+    let pixels: [u8; 21] = [
+        255, 0, 0, 255, 0, 0, 245, 0, 0, 245, 0, 0, 0, 255, 0, 0, 255, 0, 0, 240, 0,
+    ];
+    let colors = dominant_color::get_colors(&pixels);
+    assert_eq!(colors[0], dominant_color::Color { r: 250, g: 0, b: 0 });
+    assert_eq!(colors[1], dominant_color::Color { r: 0, g: 250, b: 0 });
+}
+
+#[test]
+fn image() {
+    let image = image::open(&path::Path::new("docs/Fotolia_45549559_320_480.jpg")).unwrap();
+    let colors = dominant_color::get_colors(&image.raw_pixels());
+    assert_eq!(
+        colors[0],
+        dominant_color::Color {
+            r: 232,
+            g: 230,
+            b: 228
+        }
+    );
+    assert_eq!(
+        colors[1],
+        dominant_color::Color {
+            r: 58,
+            g: 58,
+            b: 10
+        }
+    );
+    assert_eq!(
+        colors[2],
+        dominant_color::Color {
+            r: 204,
+            g: 52,
+            b: 25
+        }
+    );
+    assert_eq!(
+        colors[3],
+        dominant_color::Color {
+            r: 191,
+            g: 178,
+            b: 56
+        }
+    );
+    assert_eq!(
+        colors[4],
+        dominant_color::Color {
+            r: 104,
+            g: 152,
+            b: 12
         }
     );
 }
